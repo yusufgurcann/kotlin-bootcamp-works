7ec251815cfe2a92c1b6d03f7728c33473a4c0f1
Yusuf Gürcan <119441718+yusufgurcann@users.noreply.github.com>
2025-04-29 00:32:35 +0300

    Add Odev1

---
+++ Ödev1/app/src/main/java/com/example/odev_1/variables/Variables.kt
@@ -0,0 +1,54 @@
+package com.example.odev_1.variables
+
+fun main() {
+    val city = "Istanbul"
+    val country = "Turkey"
+    val phone = "İphone 13 PRO"
+    val postalCode = "34000"
+    val email = "yusuf@gmail.com"
+    val profession = "Software Engineer"
+    val stockQuantity = 150
+    val customerName = "Yusuf GÜRCAN"
+    val balance = 2500.75
+    val birthDate = "03/11/2000"
+    val salary = 15000
+    val maritalStatus = "Single"
+    val productReview = "Amazing product!"
+
+    val paymentDate = "05/01/2025"
+    val paymentAmount = 299.99
+    val orderQuantity = 3
+    val carModel = "BMW M5 CS"
+    val bookTitle = "Kırmızı Başlıklı Kız"
+    val publishYear = "2022"
+    val discountAmount = 20
+    val roomCount = 2
+    val latitude = 41.0082
+    val longitude = 28.9784
+    val productName = "Macbook"
+    val mealPrice = 120.0
+
+    val brand = "Samsung"
+    val musicTitle = "Shape of You"
+    val videoDuration = "3:45"
+    val productRating = 4.8
+    val imageName = "samsung.jpg"
+    val fileFormat = "JPEG"
+    val color = "Blue"
+    val colorCode = "#0000FF"
+    val phoneModel = "iPhone 15 Pro"
+    val screenSize = "6.1 inches"
+    val weight = 180.5
+
+    val nationalHoliday = "29 Ekim Cumhuriyet Bayramı"
+    val holiday = "23 Nisan"
+    val reservationDate = "06/10/2025"
+    val streetName = "Bağdat Caddesi"
+    val busLine = "16D"
+    val remainingMinutes = 15
+    val trackingCode = "TR123456789"
+    val couponDuration = "30 days"
+    val couponCode = "DISCOUNT20"
+    val billingAddress = "Besiktas, Istanbul"
+
+}
